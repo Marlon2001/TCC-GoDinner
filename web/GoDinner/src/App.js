2ddb6203fd473ef259e2a891f10b16dca3417d57
SARAH RODRIGUES SANTANA <18175198@CFP127ALUNO.LOCAL>
2019-09-02 17:00:41 -0300

    Slider pronto

--- web/GoDinner/src/App.js
+++ web/GoDinner/src/App.js
@@ -4,11 +4,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'jquery/dist/jquery.min.js';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import './recursos/css/style.css';
-// import './recursos/js/jquer';
-// import './recursos/js/owl.carousel.min.js'
 import 'popper.js/dist/popper.min.js';
 import './recursos/css/style.css';
-
 
 function App() {
   return (
--- web/GoDinner/src/componentes/globais/Slider.js
+++ web/GoDinner/src/componentes/globais/Slider.js
@@ -1,51 +1,78 @@
 import React,{Component, Fragment} from 'react';
-import Fritas from '../../recursos/icons/iconsSlider/1.png';
-import Macarrao from '../../recursos/icons/iconsSlider/2.jpg';
-import Marmita from '../../recursos/icons/iconsSlider/3.jpg';
-import Japonesa from '../../recursos/icons/iconsSlider/4.jpg';
-import Conserva from'../../recursos/icons/iconsSlider/5.jpg';
-import Carne from '../../recursos/icons/iconsSlider/6.jpg'; 
-import '../../recursos/js/jquer';
+import Fritas from './../../recursos/icons/iconsSlider/1.png';
+import Macarrao from './../../recursos/icons/iconsSlider/2.jpg';
+import Marmita from './../../recursos/icons/iconsSlider/3.jpg';
+import Japonesa from './../../recursos/icons/iconsSlider/4.jpg';
+import Conserva from'./../../recursos/icons/iconsSlider/5.jpg';
+import Carne from './../../recursos/icons/iconsSlider/6.jpg';
 
 
 
-export class ImagemSlider extends Component{
+
+ export class ImagemSlider extends Component{
     render(){
-        return(
-            <div class="item-carousel">
-                <img src={this.props.src} alt={this.props.alt} class="rounded-circle"/>
-                <p class="text-center">{this.props.titulo}</p>
-            </div>
+         return(
+//             <div className="item-carousel">
+//                 <img src={this.props.src} alt={this.props.alt} className="rounded-circle"/>
+//                 <p className="text-center">{this.props.titulo}</p>
+//             </div>
+                <div className={`carousel-item ${this.props.className}`}>
+                    <div className="row">
+                        <div className="col-3">
+                            <img class="d-block w-100 rounded-circle" src={this.props.imagem1} alt="Primeiro Slide"/>
+                            <p className="text-center">{this.props.titulo}</p>
+                        </div>
+                        <div className="col-3">
+                            <img class="d-block w-100 rounded-circle" src={this.props.imagem2} alt="Primeiro Slide"/>
+                            <p className="text-center">{this.props.titulo}</p>
+                        </div>
+                        <div className="col-3">
+                            <img class="d-block w-100 rounded-circle" src={this.props.imagem3} alt="Primeiro Slide"/>
+                            <p className="text-center">{this.props.titulo}</p>
+                        </div>
+                        <div className="col-3">
+                            <img class="d-block w-100 rounded-circle" src={this.props.imagem4} alt="Primeiro Slide"/>
+                            <p className="text-center">{this.props.titulo}</p>
+                        </div>
+                    </div>
+                </div>
         )
     }
-}
+ }
 
 
 export class SliderBox extends Component{
     render(){
         return(
-            <div class="container mt-5">
-                <h4 class="font-tamanho-23">Para todos os tipos de gostos</h4>
-                <div class="owl-carousel owl-theme">
-                    <ImagemSlider
-                        src={Fritas} alt="Fritas" titulo="Fritas"
-                    />
-                    <ImagemSlider
-                        src={Macarrao} alt="Macarrão" titulo="Macarrão"
-                    />
-                    <ImagemSlider
-                        src={Marmita} alt="Marmita" titulo="Marmita"
-                    />
-                    <ImagemSlider
-                        src={Japonesa} alt="Japonesa" titulo="Japonesa"
-                    />
-                     <ImagemSlider
-                        src={Conserva} alt="Conserva" titulo="Conserva"
-                    />
-                     <ImagemSlider
-                        src={Carne} alt="Carne" titulo="Carne"
-                    />
-                </div>
+            <div className="container mt-5">
+                <h4 className="font-tamanho-23">Para todos os tipos de gostos</h4>
+                <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                    <div class="carousel-inner">
+                        <ImagemSlider
+                            className="active"
+                            imagem1={Carne}
+                            imagem2={Fritas}
+                            imagem3={Marmita}
+                            imagem4={Japonesa}
+                        />
+                        <ImagemSlider
+                            imagem1={Macarrao}
+                            imagem2={Japonesa}
+                            imagem3={Conserva}
+                            imagem4={Fritas}
+                        />
+
+
+                    </div>
+                    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Anterior</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Próximo</span>
+                    </a>
+                    </div>
                 <hr/>
             </div>
         )
