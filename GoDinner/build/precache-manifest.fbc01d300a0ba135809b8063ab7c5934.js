40363bd6af59dda49ac791de8a8a59be0b5ce4df
SARAH RODRIGUES SANTANA <18175198@CFP127ALUNO.LOCAL>
2019-10-17 16:28:47 -0300

    login fnalizado

---
+++ GoDinner/build/precache-manifest.fbc01d300a0ba135809b8063ab7c5934.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23a69aa9563cbca538c18ed383239d4c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "24607d477d6e227837ce",
+    "url": "/static/css/2.b62f5c36.chunk.css"
+  },
+  {
+    "revision": "25ed4986d0b7f8b4e9f0",
+    "url": "/static/css/main.72510d4b.chunk.css"
+  },
+  {
+    "revision": "24607d477d6e227837ce",
+    "url": "/static/js/2.329c357e.chunk.js"
+  },
+  {
+    "revision": "25ed4986d0b7f8b4e9f0",
+    "url": "/static/js/main.20be22fe.chunk.js"
+  },
+  {
+    "revision": "007d851018e2bffad02f",
+    "url": "/static/js/runtime~main.4eaa0380.js"
+  },
+  {
+    "revision": "bbd6ecc3820f295ec3d1b174328f6fe7",
+    "url": "/static/media/1.bbd6ecc3.png"
+  },
+  {
+    "revision": "bbec41514d6e221f876ebc259e15c7b0",
+    "url": "/static/media/2.bbec4151.jpg"
+  },
+  {
+    "revision": "64525d3859e1e9141be5ba9724bf6a94",
+    "url": "/static/media/3.64525d38.jpg"
+  },
+  {
+    "revision": "926f025468e3bd3147c1afc35214f4a9",
+    "url": "/static/media/4.926f0254.jpg"
+  },
+  {
+    "revision": "9877aae493709abf2b3e1d0e83bf161d",
+    "url": "/static/media/5.9877aae4.jpg"
+  },
+  {
+    "revision": "7280ce606b4e09ffd446a5df1fe69ca3",
+    "url": "/static/media/6.7280ce60.jpg"
+  },
+  {
+    "revision": "6808a2c773223dc725531d9c9ddf5294",
+    "url": "/static/media/android.6808a2c7.png"
+  },
+  {
+    "revision": "43a0312cde2bc83fdc379aba914bc6bd",
+    "url": "/static/media/background-a-go-dinner.43a0312c.jpg"
+  },
+  {
+    "revision": "67aaafe9364f79a9d808a76edda90d0b",
+    "url": "/static/media/background-go-dinner-caracteristica.67aaafe9.jpeg"
+  },
+  {
+    "revision": "807047ef1dec47d4942f9a77afd9fea3",
+    "url": "/static/media/background-go-dinner-header.807047ef.jpg"
+  },
+  {
+    "revision": "079d2ea1d20f27306f85b4e09282ba0d",
+    "url": "/static/media/background_prato.079d2ea1.png"
+  },
+  {
+    "revision": "3e88b699b58f95f549f06b08202451be",
+    "url": "/static/media/fotoCelular.3e88b699.png"
+  },
+  {
+    "revision": "80e936f210211d94e888080b0c7f26a1",
+    "url": "/static/media/go-dinner-garfo.80e936f2.png"
+  },
+  {
+    "revision": "59f62fae6f7f23bea26da13010ba15fa",
+    "url": "/static/media/go-dinner.59f62fae.png"
+  },
+  {
+    "revision": "7856ebea3577a0d1980593769541bd53",
+    "url": "/static/media/logo.7856ebea.png"
+  }
+]);
